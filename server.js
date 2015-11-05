39a2e6cb1297eae94981e188d87a615636c03a24
Colin <colinwmackenzie@gmail.com>
2015-11-04 22:10:29 -0500

    quitting for the night

---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://colin:pw@apollo.modulusmongo.net:27017/y3Pyhoxy');
+var Ingredient = require('./app/models/ingredient');
+
+var port = process.env.PORT || 8080;
+
+var router = express.Router();
+
+router.use(function(req, res, next) {
+  console.log("Somethin' doin");
+  next();
+});
+
+router.get('/', function(req, res) {
+  res.json({ message: 'hellooo' });
+});
+
+router.route('/ingredients')
+  .post(function(req, res) {
+    var ingredient = new Ingredient();
+    ingredient.name = req.body.name; 
+
+    ingredient.save(function(err) {
+      if (err) { res.send(err); }
+
+      res.json({ message: 'Ingredient added' }); 
+    });
+  });
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('Magic happenin on ' + port);
