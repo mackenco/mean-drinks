df908ecbaf9df3f4c589ce006638734afa58b0d7
Colin <colinwmackenzie@gmail.com>
2015-11-08 21:13:34 -0500

    CRUD for ingredients

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://colin:pw@apollo.modulusmongo.net:27017/y3Pyhoxy');
+mongoose.connect('localhost');
+// mongoose.connect('mongodb://colin:pw@apollo.modulusmongo.net:27017/y3Pyhoxy');
 var Ingredient = require('./app/models/ingredient');
 
 var port = process.env.PORT || 8080;
@@ -32,6 +33,43 @@ router.route('/ingredients')
 
       res.json({ message: 'Ingredient added' }); 
     });
+  })
+
+  .get(function(req, res) {
+    Ingredient.find(function(err, ingredients) {
+      if (err) { res.send(err); }
+      res.json(ingredients); 
+    }); 
+  });
+
+router.route('/ingredients/:ingredient_id')
+  .get(function(req, res) {
+    Ingredient.findById(req.params.ingredient_id, function(err, ingredient) {
+      if (err) { res.send(err); }
+      res.json(ingredient); 
+    }); 
+  })
+
+  .put(function(req, res) {
+    Ingredient.findById(req.params.ingredient_id, function(err, ingredient) {
+      if (err) { res.send(err); }
+
+      ingredient.name = req.body.name;
+
+      ingredient.save(function(err) { 
+        if (err) { res.send(err); } 
+        res.json({ message: 'Ingredient updated!' });
+      });
+    }); 
+  })
+
+  .delete(function(req, res) {
+    Ingredient.remove({
+      _id: req.params.ingredient_id 
+    }, function(err, ingredient) {
+      if (err) { res.send(err); } 
+      res.json({ message: 'Successfully deleted' });
+    }); 
   });
 
 app.use('/api', router);
