62c94f59c4bce054b5eca7bf7fe4e34299f216c2
Colin <colinwmackenzie@gmail.com>
2015-12-01 09:26:51 -0500

    collapse id is gone

--- public/js/directives/DrinkListDirective.js
+++ public/js/directives/DrinkListDirective.js
@@ -3,20 +3,12 @@ angular.module('DrinkListDirective', [])
     return {
       restrict: 'E',
       link: function($scope) {
-        $scope.collapsedId = null;
         $scope.options = { favorite: false, unmade: false };
 
         Ingredient.get().then(function(response) {
           $scope.ingredients = response.data; 
         });
 
-        $scope.setCollapse = function(id) { 
-          if ($scope.collapsedId === id) {
-            $scope.collapsedId = null;
-          } else {
-            $scope.collapsedId = id; 
-          }
-        };
       },
       scope: {
         drinks: '=' 
