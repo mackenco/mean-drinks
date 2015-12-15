f743f789b31f937abd4e9b609b3478c17580a528
Colin <colinwmackenzie@gmail.com>
2015-12-14 23:41:28 -0500

    eod

--- public/js/controllers/DrinkCtrl.js
+++ public/js/controllers/DrinkCtrl.js
@@ -1,5 +1,6 @@
 angular.module('DrinkCtrl', [])
-  .controller('DrinkController', function($scope, Drink) {
+  .controller('DrinkController', function($scope, $rootScope, Drink) {
+    $rootScope.all = true;
     Drink.get().then(function(response) {
       $scope.drinks = response.data;
     }); 
--- public/js/directives/DrinkListDirective.js
+++ public/js/directives/DrinkListDirective.js
@@ -2,17 +2,23 @@ angular.module('DrinkListDirective', [])
   .directive('drinkList', function(Ingredient) {
     return {
       restrict: 'E',
-      link: function($scope) {
-        $scope.options = { favorite: false, unmade: false };
+      link: function(scope) {
+        scope.options = { favorite: false, unmade: false };
 
         Ingredient.get().then(function(response) {
-          $scope.ingredients = response.data; 
+          scope.ingredients = response.data; 
         });
 
+        window.logg = function() {
+          console.log(scope.all);
+        };
+
       },
-      scope: {
-        drinks: '=' 
-      },
+      scope: true,
+      // scope: {
+      //   drinks: '=' 
+      // },
+      transclude: true,
       templateUrl: 'views/_drinkList.html'
     };
   });
