b33fd9865013dd06b9789454134321dccdd8a42b
Colin <colinwmackenzie@gmail.com>
2015-12-27 22:01:22 -0500

    dinner time

--- public/js/directives/DrinkFormDirective.js
+++ public/js/directives/DrinkFormDirective.js
@@ -3,7 +3,19 @@ angular.module('DrinkFormDirective', [])
     return {
       restrict: 'E',
       link: function(scope) {
-      
+        scope.addOrEditButton = function() {
+          return $rootScope.editingDrink ? "Edit" : "Add"; 
+        };
+
+        scope.addOrEdit = function(drink) {
+          drink.ingredients = drink.ingredients.split(",");
+          if ($rootScope.editingDrink) {
+            console.log(drink);
+            console.log(drink.ingredients); 
+          } else {
+          
+          }
+        };
       },
       templateUrl: 'views/_drinkForm.html' 
     };
